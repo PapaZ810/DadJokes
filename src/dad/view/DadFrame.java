2d0559df9a4093942cc0c3e6b7487130808f6f83
PapaZ810 <zombiezac@att.net>
2019-06-08 09:51:51 -0600

    more setup things

--- src/dad/view/DadFrame.java
+++ src/dad/view/DadFrame.java
@@ -2,22 +2,30 @@ package dad.view;
 
 import javax.swing.*;
 import dad.controller.DadController;
+import java.awt.*;
 
 public class DadFrame extends JFrame
 {
 	private DadController app;
+	private DadPanel pane;
 	
 	public DadFrame(DadController app)
 	{
 		super();
 		
 		this.app = app;
+		pane = new DadPanel(app);
 		
 		setupFrame();
 	}
 	
 	private void setupFrame()
 	{
-		
+		this.setContentPane(pane);
+		this.setTitle("I'm a Dad");
+		this.setPreferredSize(new Dimension(400, 200));
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.setVisible(true);
 	}
 }
